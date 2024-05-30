4118d9550153d9650dbe76f84e077bff40d53a5b
Siyabonga Masuku <siya.coma@gmail.com>
2024-05-30 12:32:41 +0200

    modified vite file

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,8 @@ import handlebars from 'vite-plugin-handlebars';
 import data from './data.json';
 
 export default {
-  plugins: [
-    handlebars({
-      context: {}, // replace the curly brackets with data.json
-    }),
-  ],
+  base : '/responsive_cv/',   // <-- add this
+  plugins: [handlebars({
+      context : data   // add this
+  })],
 };
