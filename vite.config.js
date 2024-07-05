da02bf622dd94fb7859dae4f81a18d60fc32d03e
Siyabonga Masuku <siya.coma@gmail.com>
2024-07-06 00:03:06 +0200

    first commit

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import handlebars from 'vite-plugin-handlebars';
-import data from './data.json';
-
-export default {
-  base : '/responsive_cv/',   // <-- add this
-  plugins: [handlebars({
-      context : data   // add this
-  })],
-};
